93adf715be179c07ef55020c7c74e083684d0548
madura <madura@sqrmobile.com>
2021-06-09 12:49:40 +0530

    added test file

--- Gemfile
+++ Gemfile
@@ -7,3 +7,7 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem "http", "~> 5.0"
+
+group :test do
+gem "minitest", "~> 5.14"
+end
---
+++ test/test_shoutout.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'shoutout'
+
+class ShoutoutTest < Minitest::Test
+    def setup
+        @shoutout = Shoutout.new("APIKEY HERE")
+    end
+    def test_send_sms
+        response=@shoutout.sendSms(from:"ShoutTEST",to:"94778845713",body:"This is a test message")
+        puts response
+        assert_equal "1001",
+          response["status"]
+    end
+end
