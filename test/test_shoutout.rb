b11dbe95d4e417c760c82f7db4fc3136d3912dc5
madura <madura@sqrmobile.com>
2021-06-18 13:39:36 +0530

    update test case

--- test/test_shoutout.rb
+++ test/test_shoutout.rb
@@ -3,7 +3,7 @@ require 'shoutout'
 
 class ShoutoutTest < Minitest::Test
     def setup
-        @shoutout = .new("APIKEY HERE")
+        @shoutout = Shoutout::Client.new("APIKEY HERE")
     end
     def test_send_sms
         response=@shoutout.sendSms(from:"ShoutTEST",to:"94778845713",body:"This is a test message")
